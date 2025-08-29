eea8d3d7322546fb7320b8b806a0dbc1d667d11c
Akshitha-123 <akshitha1302@gmail.com>
2025-08-28 20:20:29 -0400

    Updated code, image; ignored credentials

--- analyze.py
+++ analyze.py
@@ -2,11 +2,32 @@ from google import genai
 from PIL import Image
 import io
 import os
+from dotenv import load_dotenv
 
-gemini_api_key = 'HARDCODED_GEMINI_API_KEY_HERE'
+load_dotenv('cred.env')
+gemini_api_key = os.getenv('gemini_api_key')
 gemini_client = genai.Client(api_key=gemini_api_key)
 
 def get_llm_response(image_data: bytes) -> str:
     image = Image.open(io.BytesIO(image_data))
-    # implement the call to the Gemini API here
-    # docs: https://ai.google.dev/gemini-api/docs/text-generation
+    image = image.convert("RGB")
+
+    # Instantiate the GenerativeModel object
+    
+
+    prompt_text = (
+        "Analyze the image and describe the mood in a few sentences."
+        "How is the mood of the image?"
+        "Is the mood of the image sad or happy or what else?"
+    )
+
+    # Use generate_content() to handle both text and image
+    response = gemini_client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents=[image, prompt_text]
+)
+
+    # The response text is now accessed via the .text attribute
+    generated_text = response.text
+
+    return generated_text
